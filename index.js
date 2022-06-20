7cb65e0820f67fd15ce88ac2c09c2a2c45c90bc4
gusdinizmaia <gustavodiniz37@hotmail.com>
2022-06-20 14:23:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome= 'Jubileu'
+let sobrenome= 'Micalateia'
+let cidade= 'Quixeramobim'
+let anoDeNascimento= 1949
+
+console.log(`Eu sou ${nome} ${sobrenome}, tenho ${2022-anoDeNascimento} e moro em ${cidade}`)
